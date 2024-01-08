4934521b1a0b0b2ba681d8f4843947195854c094
Morteza <lmortezal.world@gmail.com>
2024-01-08 21:42:13 +0330

    get link home page

--- web-crawler.py
+++ web-crawler.py
@@ -3,13 +3,13 @@ import scrapy
 
 class MyCrawler(scrapy.Spider):
     name = "quotes"
-    start_urls = ["https://nbcnews.com"]
+    start_urls = ["https://edition.cnn.com"]
 
     def parse(self, response):
-        for related_content in response.css('div.related-content-tease'):
-            link = related_content.css('a::attr(href)').extract_first()
-            text = related_content.css('.related-content-tease__headline::text').extract_first()
+        for related_content in response.xpath('//div[contains(@class, "card container__item")]'):
             yield {
-                'link': link,
-                'text': text,
+            #    'full': related_content.css('*').get()
+                'span': related_content.css('span::text').get(),
+                'a': "https://edition.cnn.com" + related_content.css('a::attr(href)').get()
             }
+        
