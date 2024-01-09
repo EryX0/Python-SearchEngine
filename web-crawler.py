452ad0dfe24d2accff1f1eb4104fd6e3114cc1ec
Morteza <lmortezal.world@gmail.com>
2024-01-09 14:25:48 +0330

    add limit variable -- Checking the article whether it is empty or not -- Convert title and article from array to string

--- web-crawler.py
+++ web-crawler.py
@@ -1,31 +1,48 @@
 import scrapy
 from urllib.parse import urljoin
-
-x = 0
+from scrapy.exceptions import CloseSpider
+import re
+from unidecode import unidecode
 
 class MyCrawler(scrapy.Spider):
     name = "quotes"
-    start_urls = ["https://edition.cnn.com"]
+    start_urls = ["https://edition.cnn.com","https://edition.cnn.com/world/middle-east","https://edition.cnn.com/style/architecture","https://edition.cnn.com/business/tech"]
+    count = 0
+    limit = 50  # Set your limit here
+
     def parse(self, response):
         for related_content in response.xpath('//div[contains(@class, "card container__item")]'):
             link = urljoin("https://edition.cnn.com",related_content.css('a::attr(href)').get())
-            yield scrapy.Request(url=link, callback=self.parse_link, meta={'link': link})
-            if x_add() == 2:
-                print('Done!')
-                break
-    
-    
+            yield scrapy.Request(url=link, callback=self.parse_link, meta={'link': link}, dont_filter=False)
+
     def parse_link(self,response):
+        
         link = response.meta.get('link')
-        title = response.css('div.headline__wrapper ::text').extract()
-        article = response.css('div.article__content ::text').extract()
+        title = (''.join([text.strip() for text in response.css('div.headline__wrapper ::text').extract() if text.strip()])).encode('ascii', 'ignore').decode('ascii')
+        article = (''.join([text.strip() for text in response.css('div.article__content ::text').extract() if text.strip()])).encode('ascii', 'ignore').decode('ascii')
+        
+        if not article.strip():
+            self.log(f"Skipped {link} - Empty article")
+            self.count -= 1
+            return
+        
+        if self.count < self.limit:
+            self.count += 1
+            print(self.count)
+        else:
+            raise CloseSpider('Limit reached')
+            print(self.count , "finsih -----------------")
+
         yield {
             'link': link,
             'title': title,
             'article': article
         }
 
-def x_add():
-    global x
-    x = x + 1
-    return x
+
+        for related_link in response.css('a.related-content__link ::attr(href)'):
+            link = urljoin("https://edition.cnn.com",related_link.get())
+            yield scrapy.Request(url=link, callback=self.parse_link, meta={'link': link}, dont_filter=False)
+
+
+    
