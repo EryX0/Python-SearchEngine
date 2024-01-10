ec0eb173beec0ccfae7cefb9cd32df37c0d32458
EryX <rezahn81@gmail.com>
2024-01-10 21:00:24 +0330

    implemented tfidf and vsm functions in the code

--- main.py
+++ main.py
@@ -23,13 +23,15 @@ def load_data(file_path, num_rows=500):
 
 # Preprocess data (tokenization, stop-word removal, and stemming)
 def preprocess_data(data):
+
     stop_words = set(stopwords.words('english'))
     ps = PorterStemmer()
 
     processed_data = []
     for document in data:
         tokens = word_tokenize(document.lower())
-        filtered_tokens = [ps.stem(token) for token in tokens if token.isalnum() and token not in stop_words]
+        # make sure there are no numbers
+        filtered_tokens = [ps.stem(token) for token in tokens if token.isalnum() and token not in stop_words and not token.isdigit()]
         processed_data.append(" ".join(filtered_tokens))
 
     return processed_data
@@ -56,14 +58,37 @@ def boolean_retrieval(query, index, processed_data):
 
     return result_set
 
+def tfidf_retrieval(query, processed_data):
+
+    vectorizer = TfidfVectorizer(max_df=1.0,min_df=0.0)
+    vectors = vectorizer.fit_transform(processed_data)
+
+    feature_names = vectorizer.get_feature_names_out()
+
+    # get the index of the query terms in the feature names
+    query_indexes = []
+    for query_term in query.split():
+        query_indexes.append(feature_names.tolist().index(query_term))
+
+    # sum query indexes scores of each document, and store in query_scores
+    query_scores = []
+    for document in vectors:
+        score = 0
+        for index in query_indexes:
+            score += document[0, index]
+        query_scores.append(score)
+
+    return query_scores
+
 # Vector-based Retrieval using TF-IDF
 def vector_based_retrieval(query, processed_data):
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform(processed_data)
+    vectorizer = TfidfVectorizer(max_df=1.0,min_df=0.0)
+    vectors = vectorizer.fit_transform(processed_data)
     query_vector = vectorizer.transform([query])
+    #vsm retrieval with cosine similarity
+    similarity_score = cosine_similarity(query_vector, vectors)
 
-    similarity_scores = cosine_similarity(query_vector, tfidf_matrix).flatten()
-    return similarity_scores
+    return similarity_score
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -81,7 +106,8 @@ def search():
     index = create_index(processed_data)
 
     # Get query from the URL parameter
-    user_query = request.args.get('query')
+    #user_query = request.args.get('query')
+    user_query = "jesus god"
 
     # Stemming on the query
     user_query = " ".join([PorterStemmer().stem(word) for word in word_tokenize(user_query.lower())])
@@ -89,16 +115,20 @@ def search():
     # Boolean Retrieval
     boolean_results = list(boolean_retrieval(user_query, index, processed_data))
 
-    # Vector-based Retrieval using TF-IDF
-    tfidf_scores = vector_based_retrieval(user_query, processed_data)
+    #tfidf retrieval function calling here.
+    tfidf_scores = tfidf_retrieval(user_query, processed_data)
+
+    #vsm retrieval function calling here.
+    vsm_scores = vector_based_retrieval(user_query, processed_data)
 
     # Combine results, scores, and document count
     results = [
         {
             "id": data[i]['id'],
             "document_number": i + 1,  # Add 1 to start counting from 1
+            "tfidf_score": tfidf_scores[i],
+            "vsm_score": vsm_scores[0][i],
             "highlights": data[i]['highlights'],
-            "score": tfidf_scores[i],
             "article": data[i]['article']
         }
         for i in boolean_results
